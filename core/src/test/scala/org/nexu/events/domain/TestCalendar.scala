a5027941bcbc465ffd792e2ffce9000093ede40f
Cyril Collen <cyril@nexu.org>
2015-12-20 12:59:16 +0100

    Fixed unit test

--- core/src/test/scala/org/nexu/events/domain/TestCalendar.scala
+++ core/src/test/scala/org/nexu/events/domain/TestCalendar.scala
@@ -1,9 +1,9 @@
 package org.nexu.events.domain
 
-import org.joda.time
-import org.joda.time.DateTime
-import org.nexu.events.command.{OptimizeTimetable, CreateMeeting}
-import org.nexu.events.event.{TimetableReorganized, MeetingCreated}
+import java.time.{Duration, LocalDateTime}
+
+import org.nexu.events.command.{CreateMeeting, OptimizeTimetable}
+import org.nexu.events.event.{MeetingCreated, TimetableReorganized}
 import org.scalatest.FunSuite
 
 /**
@@ -16,7 +16,7 @@ class TestCalendar extends FunSuite {
   test("Add an appointment to my calendar") {
     // Given an empty calendar
     val calendar = new Calendar(new User("cyril@test.org", "sX"), List(), "ProCalendar", 0)
-    val meeting: Meeting = new Meeting(new TimeSlot(DateTime.now(), time.Duration.standardHours(1)), "It's important", List())
+    val meeting: Meeting = new Meeting(new TimeSlot(LocalDateTime.now(), Duration.ofHours(1)), "It's important", List())
 
     // When meeting is added to calendar
     val event = calendar.onCommand.applyOrElse(
@@ -34,18 +34,18 @@ class TestCalendar extends FunSuite {
   test("Test re-organized meeting nominal case") {
     // Given the following meetings
     val meetings = List(
-      new Meeting(new TimeSlot(DateTime.parse("2014-01-01T02:00"), time.Duration.standardHours(2)), "It's important", List()),
-      new Meeting(new TimeSlot(DateTime.parse("2014-01-01T02:00"), time.Duration.standardHours(1)), "It's important", List()),
-      new Meeting(new TimeSlot(DateTime.parse("2014-01-01T03:00"), time.Duration.standardHours(1)), "It's important", List()),
-      new Meeting(new TimeSlot(DateTime.parse("2014-01-01T04:00"), time.Duration.standardHours(3)), "It's important", List()),
-      new Meeting(new TimeSlot(DateTime.parse("2014-01-01T04:00"), time.Duration.standardHours(2)), "It's important", List()))
+      new Meeting(new TimeSlot(LocalDateTime.parse("2014-01-01T02:00"), Duration.ofHours(2)), "It's important", List()),
+      new Meeting(new TimeSlot(LocalDateTime.parse("2014-01-01T02:00"), Duration.ofHours(1)), "It's important", List()),
+      new Meeting(new TimeSlot(LocalDateTime.parse("2014-01-01T03:00"), Duration.ofHours(1)), "It's important", List()),
+      new Meeting(new TimeSlot(LocalDateTime.parse("2014-01-01T04:00"), Duration.ofHours(3)), "It's important", List()),
+      new Meeting(new TimeSlot(LocalDateTime.parse("2014-01-01T04:00"), Duration.ofHours(2)), "It's important", List()))
 
 
     val calendar = new Calendar(new User("cyril@test.org", "sX"), meetings, "ProCalendar", 0)
 
     // When reorganized calendar for the 01-01-2014
     val event = calendar.onCommand.applyOrElse(
-      new OptimizeTimetable("ProCalendar", DateTime.parse("2014-01-01T02:00"), DateTime.parse("2014-01-02T02:00"))
+      new OptimizeTimetable("ProCalendar", LocalDateTime.parse("2014-01-01T02:00"), LocalDateTime.parse("2014-01-02T02:00"))
       , null)
 
     // Then
--- core/src/test/scala/org/nexu/events/domain/TestTimeSlot.scala
+++ core/src/test/scala/org/nexu/events/domain/TestTimeSlot.scala
@@ -3,7 +3,6 @@ package org.nexu.events.domain
 import java.time
 import java.time.LocalDateTime
 
-import org.joda.time.{Duration, DateTime}
 import org.scalatest.FunSuite
 
 /**
@@ -11,43 +10,42 @@ import org.scalatest.FunSuite
  */
 class TestTimeSlot extends FunSuite {
 
-
   test("Time is not overlapping as it's in the past") {
-    val referenceTimeSlot: TimeSlot = new TimeSlot(LocalDateTime.parse("2010-06-30T01:20+02:00"), time.Duration.ofHours(1))
+    val referenceTimeSlot: TimeSlot = new TimeSlot(LocalDateTime.parse("2010-06-30T01:20"), time.Duration.ofHours(1))
     val isOverlapping: Boolean = referenceTimeSlot
-      .isOverlappingWith(new TimeSlot(LocalDateTime.parse("2010-06-30T00:20+02:00"), time.Duration.ofHours(1)))
+      .isOverlappingWith(new TimeSlot(LocalDateTime.parse("2010-06-30T00:20"), time.Duration.ofHours(1)))
 
     assert(!isOverlapping, "00h20 -> 01h20 is not overlapping with 01h20 -> 02h20")
   }
 
   test("Time is not overlapping as it's in the future") {
-    val referenceTimeSlot: TimeSlot = new TimeSlot(LocalDateTime.parse("2010-06-30T01:20+02:00"), time.Duration.ofHours(1))
+    val referenceTimeSlot: TimeSlot = new TimeSlot(LocalDateTime.parse("2010-06-30T01:20"), time.Duration.ofHours(1))
     val isOverlapping: Boolean = referenceTimeSlot
-      .isOverlappingWith(new TimeSlot(LocalDateTime.parse("2010-06-30T02:20+02:00"), time.Duration.ofHours(1)))
+      .isOverlappingWith(new TimeSlot(LocalDateTime.parse("2010-06-30T02:20"), time.Duration.ofHours(1)))
 
     assert(!isOverlapping, "02h20 -> 03h20 is not overlapping with 01h20 -> 02h20")
   }
 
   test("Start Time outside of the lower bound of the timeslot. Ending time inside") {
-    val referenceTimeSlot: TimeSlot = new TimeSlot(LocalDateTime.parse("2010-06-30T01:20+02:00"), time.Duration.ofHours(1))
+    val referenceTimeSlot: TimeSlot = new TimeSlot(LocalDateTime.parse("2010-06-30T01:20"), time.Duration.ofHours(1))
     val isOverlapping: Boolean = referenceTimeSlot
-      .isOverlappingWith(new TimeSlot(LocalDateTime.parse("2010-06-30T01:00+02:00"), time.Duration.ofHours(1)))
+      .isOverlappingWith(new TimeSlot(LocalDateTime.parse("2010-06-30T01:00"), time.Duration.ofHours(1)))
 
     assert(isOverlapping, "01:20 until 02:00 is overlapping")
   }
 
   test("End Time outside of the higher bound of the timeslot. Start time inside") {
-    val referenceTimeSlot: TimeSlot = new TimeSlot(LocalDateTime.parse("2010-06-30T01:20+02:00"), time.Duration.ofHours(1))
+    val referenceTimeSlot: TimeSlot = new TimeSlot(LocalDateTime.parse("2010-06-30T01:20"), time.Duration.ofHours(1))
     val isOverlapping: Boolean = referenceTimeSlot
-      .isOverlappingWith(new TimeSlot(LocalDateTime.parse("2010-06-30T02:00+02:00"), time.Duration.ofHours(1)))
+      .isOverlappingWith(new TimeSlot(LocalDateTime.parse("2010-06-30T02:00"), time.Duration.ofHours(1)))
 
     assert(isOverlapping, "02:00 until 02:20 is overlapping")
   }
 
   test("One timeslot inside another one") {
-    val referenceTimeSlot: TimeSlot = new TimeSlot(LocalDateTime.parse("2010-06-30T01:20+02:00"), time.Duration.ofHours(2))
+    val referenceTimeSlot: TimeSlot = new TimeSlot(LocalDateTime.parse("2010-06-30T01:20"), time.Duration.ofHours(2))
     val isOverlapping: Boolean = referenceTimeSlot
-      .isOverlappingWith(new TimeSlot(LocalDateTime.parse("2010-06-30T01:30+02:00"), time.Duration.ofHours(1)))
+      .isOverlappingWith(new TimeSlot(LocalDateTime.parse("2010-06-30T01:30"), time.Duration.ofHours(1)))
 
     assert(isOverlapping, "The second timeslot is fully overlapping")
   }
